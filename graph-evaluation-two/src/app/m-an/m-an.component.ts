90f3fe90a3ca6c9f2e7b16e9935654587e4724ff
velitchko <velitchkofilipov@gmail.com>
2021-04-04 09:50:53 +0200

    matrix animation component

--- graph-evaluation-two/src/app/m-an/m-an.component.ts
+++ graph-evaluation-two/src/app/m-an/m-an.component.ts
@@ -1,15 +1,196 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import * as d3 from 'd3';
+import { DataService, Graph } from '../data.service';
+import { WIDTH, HEIGHT, CELL_SIZE, ANIMATION_DURATION, SVG_MARGIN, FONT_SIZE } from '../config';
+import { Options } from '@angular-slider/ngx-slider';
+import { Node, Link, Cell } from '../node-link';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-m-an',
   templateUrl: './m-an.component.html',
   styleUrls: ['./m-an.component.scss']
 })
-export class MAnComponent implements OnInit {
+export class MAnComponent implements OnInit, AfterViewInit {
+  @ViewChild('container') container: ElementRef;
+  private graph: Graph;
 
-  constructor() { }
+  private matrix: Array<Cell>;
 
-  ngOnInit(): void {
+  private svgContainer: d3.Selection<SVGElement, {}, HTMLElement, any>;
+  private g: d3.Selection<SVGGElement, {}, HTMLElement, any>;
+  private cells: d3.Selection<any, {}, any, any>;
+  
+  private width: number;
+  private height: number;
+
+  private time: number;
+  private animationHandle: any; // animation timer handler
+
+  value: number = 1;
+  options: Options = {
+    floor: 1,
+    ceil: 4
+  };
+
+  constructor(private ds: DataService, private route: ActivatedRoute) { 
+    this.matrix = new Array<Cell>();
+    this.time = 0;
   }
 
+  ngOnInit(): void {
+    this.route.queryParams
+    .subscribe(params => {
+      const graph = params['graph'];
+      this.graph = this.ds.getGraph(graph);
+    });
+  }
+
+  ngAfterViewInit(): void {
+    this.width = (this.container.nativeElement as HTMLElement).offsetWidth;
+    this.height = (this.container.nativeElement as HTMLElement).offsetHeight;
+
+    if(this.graph) {
+      this.graph.links.forEach((l: Link<Node>) => {
+        let sNode = this.graph.nodes.find((n: Node) => { return n.id === l.source; });
+        let tNode = this.graph.nodes.find((n: Node) => { return n.id === l.target; });
+        l.source = sNode;
+        l.target = tNode;
+      });
+      this.setup();
+      this.init();
+    }
+  }
+
+  
+  stop(): void {
+    clearTimeout(this.animationHandle);
+  }
+
+  start(): void {
+    console.log('start animation');
+    this.animate();
+  }
+
+  animate(): void {
+    this.update(this.time);
+    this.time === 4 ? this.time = 0 : this.time++;
+    this.animationHandle = setTimeout(this.animate.bind(this), ANIMATION_DURATION);
+  }
+
+
+  setup(): void {
+    this.svgContainer = (d3.select('#svg-container-man') as any)
+    .append('svg')
+    .attr('viewBox', [0, 0, this.width, this.height])
+    .attr('width', this.width)
+    .attr('height', this.height);
+
+    this.g = this.svgContainer.append('g')
+                .attr('transform', `translate(${SVG_MARGIN.left}, ${SVG_MARGIN.top})`);
+
+    this.cells = this.g.append('g').attr('class', 'cells').selectAll('.cell');
+  }
+
+  init(): void {
+    let edgeHash = new Map<string, any>();
+    this.graph.links
+    .map((l: Link<Node>) => { return { source: l.source, target: l.target }; })
+    .forEach((link: Link<Node>) => {
+      // Undirected graph - duplicate link s-t && t-s
+      let idA: string, idB: string = '';
+      if(link.source === link.target) return;
+      idA = `${(link.source as Node).label}-${(link.target as Node).label}`;
+      idB = `${(link.target as Node).label}-${(link.source as Node).label}`;
+
+      edgeHash.set(idA, link);
+      edgeHash.set(idB, link);
+    });
+
+    this.graph.nodes.forEach((source: Node, sourceId: number) => {
+      this.graph.nodes.forEach((target: Node, targetId: number) => {
+        let cell = {
+          id: `${source.label}-${target.label}`,
+          x: targetId,
+          y: sourceId,
+          link: 0,
+          time: source.time
+        };
+        if (edgeHash.has(cell.id)) cell.link = 1; 
+        this.matrix.push(cell);
+      });
+    });
+    this.render();
+  }
+
+  update($event: number): void {
+    // CELLS
+    this.cells
+    .transition()
+    .duration(ANIMATION_DURATION)
+    .ease(d3.easeCubicOut)
+    .attr('fill-opacity', (d: any) => { 
+      console.log(d);
+      return d.link ? (d.time === $event ? 1 : 0) : 0;
+    });
+  }
+
+  render(): void {
+    this.g.selectAll('.rows').remove();
+    this.g.selectAll('.columns').remove();
+
+    // UPDATE
+    this.cells = this.cells.data(this.matrix);
+      
+    // ENTER 
+    this.cells = this.cells
+      .enter()
+      .append('rect')
+      .attr('class', 'cell');
+
+    // JOIN
+    this.cells 
+      .attr('width', CELL_SIZE)
+      .attr('height', CELL_SIZE)
+      .attr('x', (d: any) => { return d.x * CELL_SIZE; })
+      .attr('y', (d: any) => { return d.y * CELL_SIZE; })
+      .attr('fill-opacity', (d: any) => { return d.link ? 1 : 0; })
+      .attr('fill', (d: any) => { return 'darkgray'; })
+      .attr('stroke', '#999')
+      .attr('stroke-width', '1px')
+      .attr('stroke-opacity', .25)
+      .merge(this.cells);
+    
+    // EXIT
+    this.cells.selectAll('.cell').remove();
+
+    // ROWS
+    this.g.append('g')
+      .attr('class', 'rows')
+      .selectAll('text')
+      .data(this.graph.nodes)
+      .enter()
+      .append('text')
+      .attr('y', (d: any, i: number) => {
+        return i * CELL_SIZE + CELL_SIZE;
+      })
+      .text((d: any) => { return d.label; })
+      .attr('text-anchor', 'end')
+      .attr('font-size', FONT_SIZE);
+
+    // COLUMNS
+    this.g.append('g')
+      .attr('class', 'columns')
+      .selectAll('text')
+      .data(this.graph.nodes)
+      .enter()
+      .append('text')
+      .attr('transform', 'rotate(-90)') // Due to rotation X is now Y
+      .attr('y', (d: any, i: number) => {
+        return i * CELL_SIZE + CELL_SIZE;
+      })
+      .text((d: any) => { return d.label; })
+      .attr('text-anchor', 'start')
+      .attr('font-size', FONT_SIZE);
+  }
 }
