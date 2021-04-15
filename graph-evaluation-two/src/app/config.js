566e072f321376f28eba571eb9612fc8227da0a2
velitchko <velitchkofilipov@gmail.com>
2021-04-15 11:56:57 +0200

    more configuration options added

--- graph-evaluation-two/src/app/config.js
+++ graph-evaluation-two/src/app/config.js
@@ -1,15 +1,48 @@
 export const WIDTH = 960;
 export const HEIGHT = 960;
+export const NUMBER_OF_TIME_SLICES = 4;
+
 export const NODE_SIZE = 8;
 export const LINK_LENGTH = 20;
+
 export const TRANSITION_DURATION = 750;
+
 export const ANIMATION_DURATION = 5000;
 export const ANIMATION_INCREMENT = 1000;
 export const ANIMATION_UPPER_BOUND = 10000;
 export const ANIMATION_LOWER_BOUND = 1000;
+
 export const CELL_SIZE = 16;
 export const FONT_SIZE = 18;
-export const NUMBER_OF_TIME_SLICES = 4;
+
+export const MATRIX_SIZE = {
+    WIDTH: 100, // placeholder
+    HEIGHT: 100 // placeholder
+};
+
+export const NODE_LINK_SIZE = {
+    WIDTH: 100, // placeholder
+    HEIGHT: 100 // placeholder
+};
+
+export const SIMULATION_CONFIGURATION = {
+    LINK_DISTANCE: 1, // placeholder
+    LINK_STRENGTH: 1, // placeholder
+    NODE_STRENGTH: 1, // placeholder
+    MANYBODY_STRENGTH: 1 // placeholder
+};
+
+export const DISPLAY_CONFIGURATION = {
+    LINK_WIDTH: 1, // placeholder
+    LINK_OPACITY_DEFAULT: 1, // placeholder
+    LINK_OPACITY_HIDDEN: 0, // placeholder
+    LINK_OPACITY_FADED: 0.25,  // placeholder
+    NODE_RADIUS: 1, // placeholder
+    NODE_BORDER: 1,  // placeholder
+    ROW_COL_HIGHLIGHT_COLOR: '#ff0000', // placeholder
+    CELL_BORDER_SIZE: 1 // placeholder
+};
+
 export const SVG_MARGIN = {
     top: 50, 
     right: 20, 
