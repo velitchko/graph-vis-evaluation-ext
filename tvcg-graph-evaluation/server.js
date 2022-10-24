3b800209109a7f8c0cca13537b2b45ea4b2d5ef6
velitchko <velitchko.filipov@tuwien.ac.at>
2022-10-24 10:33:59 +0200

    update dist directory with new project name

--- tvcg-graph-evaluation/server.js
+++ tvcg-graph-evaluation/server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/graph-evaluation-two'));
+app.use(express.static(__dirname + '/dist/tvcg-graph-evaluation'));
 
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/graph-evaluation-two/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/tvcg-graph-evaluation/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
