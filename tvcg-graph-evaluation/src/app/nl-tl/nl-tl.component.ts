20f97b51b6fd319d9f2905630dc0751545dff855
velitchko <velitchko.filipov@tuwien.ac.at>
2022-10-24 10:31:39 +0200

    renaming project + removing SI

--- tvcg-graph-evaluation/src/app/nl-tl/nl-tl.component.ts
+++ tvcg-graph-evaluation/src/app/nl-tl/nl-tl.component.ts
@@ -2,17 +2,17 @@ import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angula
 import { ActivatedRoute } from '@angular/router';
 import * as d3 from 'd3';
 import { DataService, Graph } from '../data.service';
-import { NODE_LINK_SIZE, DISPLAY_CONFIGURATION, SIMULATION_CONFIGURATION, FONT_SIZE, NUMBER_OF_TIME_SLICES } from '../config';
+import { NODE_LINK_SIZE, DISPLAY_CONFIGURATION, SIMULATION_CONFIGURATION, TRANSITION_DURATION } from '../config';
 import { Options } from '@angular-slider/ngx-slider';
 import { Node, Link } from '../node-link';
 import { HttpClient } from '@angular/common/http';
 @Component({
-  selector: 'app-nl-si',
-  templateUrl: './nl-si.component.html',
-  styleUrls: ['./nl-si.component.scss']
+  selector: 'app-nl-tl',
+  templateUrl: './nl-tl.component.html',
+  styleUrls: ['./nl-tl.component.scss']
 })
 
-export class NlSiComponent implements OnInit, AfterViewInit {
+export class NlTlComponent implements OnInit, AfterViewInit {
   @ViewChild('container') container: ElementRef;
 
   private graph: Graph;
@@ -20,15 +20,11 @@ export class NlSiComponent implements OnInit, AfterViewInit {
 
   private svgContainer: d3.Selection<SVGElement, {}, HTMLElement, any>;
   private g: d3.Selection<SVGGElement, {}, HTMLElement, any>;
-  private legend: d3.Selection<any, {}, any, any>;
 
   private nodes: d3.Selection<any, {}, any, any>;
   private links: d3.Selection<any, {}, any, any>;
 
   private simulation: d3.Simulation<Node, Link<Node>>;
-
-  // Color Range
-  private color: d3.ScaleSequential<string, never>;
 
   private zoom: d3.ZoomBehavior<any, {}>;
   private zoomStartTime: number;
@@ -39,25 +35,32 @@ export class NlSiComponent implements OnInit, AfterViewInit {
   private dragEndTime: number;
 
   private timers: Array<{ type: string, time: number }>; // interaction type + time in seconds
-  private interactions: { zooms: number, drags: number }; // number of zooms, drags
+  private interactions: { zooms: number, drags: number, slider: number }; // number of zooms, drags
 
   private width: number;
   private height: number;
 
-  value: number = 1;
+  sliderWidth: string;
 
+  value: number = 1;
   options: Options = {
     floor: 1,
-    ceil: 4
+    ceil: 4,
+    ticksArray: [1, 2, 3, 4],
+    translate: (value: number): string => {
+      return `Time: ${value}`;
+    }
   };
 
   constructor(private ds: DataService, private route: ActivatedRoute, private http: HttpClient) {
     this.timers = new Array<{ type: string, time: number }>();
     this.interactions = {
       zooms: 0,
-      drags: 0
+      drags: 0,
+      slider: 0
     };
     this.interactionSwitch = false;
+    this.sliderWidth = `${NODE_LINK_SIZE.WIDTH}px`;
   }
 
   ngOnInit(): void {
@@ -147,29 +150,7 @@ export class NlSiComponent implements OnInit, AfterViewInit {
     parent.postMessage({ interactions: this.interactions, timers: this.timers }, '*');
   }
 
-  mouseOver($event: Event): void {
-    if (!this.interactionSwitch) return; // no interaction for you
-
-    const time = +($event.currentTarget as SVGElement).getAttribute('time');
-    
-    if(!time) return;
-    
-    // this.links
-    //   .transition()
-    //   .duration(TRANSITION_DURATION/2)
-    //   .attr('stroke-opacity', (d: Link<Node>) => {
-    //     return d.time[0] === time ? 1 : 0.125;
-    //   });
-  }
-
-  mouseOut($event: Event): void {
-    if (!this.interactionSwitch) return; // no interaction for you
-    // this.links
-    // .transition()
-    // .duration(TRANSITION_DURATION/2)
-    // .attr('stroke-opacity', 1);
-  }
-
+  
   zoomFit() {
     const bounds = (this.svgContainer.node() as any).getBBox();
     
@@ -178,23 +159,16 @@ export class NlSiComponent implements OnInit, AfterViewInit {
     
     const width = bounds.width;
     const height = bounds.height;
-    
+
     const midX = bounds.x + width / 2;
     const midY = bounds.y + height / 2;
-
+  
     if (width == 0 || height == 0) return; // nothing to fit
 
-    const scale = 0.95 / Math.max(width / fullWidth, height / fullHeight);
+    const scale = 0.8 / Math.max(width / fullWidth, height / fullHeight);
     const translate = [fullWidth / 2 - scale * midX, fullHeight / 2 - scale * midY];
-    
-    const transform = d3.zoomIdentity
-    .translate(0, 50)
-    .scale(scale);
 
-    this.g
-    .transition()
-    .duration(0) // milliseconds
-    .call(this.zoom.transform, transform);
+    this.g.attr('transform', `scale(${scale}) translate(${translate[0] - 100}, 50)`);
 }
 
   setup(): void {
@@ -204,41 +178,22 @@ export class NlSiComponent implements OnInit, AfterViewInit {
       .on('zoom', this.zooming.bind(this))
       .on('end', this.zoomEnd.bind(this));
 
-    this.color = d3.scaleSequential(d3.interpolateViridis).domain([1, NUMBER_OF_TIME_SLICES]);
-
     this.drag = d3.drag()
       .on('start', this.dragStart.bind(this))
       .on('drag', this.dragging.bind(this))
       .on('end', this.dragEnd.bind(this));
 
-    this.svgContainer = (d3.select('#svg-container-nlsi') as any)
+    this.svgContainer = (d3.select('#svg-container-nltl') as any)
       .append('svg')
       .attr('viewBox', [0, 0, this.width, this.height])
       .attr('width', this.width)
       .attr('height', this.height)
       .call(this.zoom);
 
-    this.legend = this.svgContainer.append('g')
-      .attr('class', 'legend')
-      .selectAll('rect');
-
     this.g = this.svgContainer.append('g');
 
-    // Linearize graph links
-    const tempGraphLinks = this.graph.links;
-    this.graph.links = new Array<{ source: number; target: number; time: Array<number> }>();
-    tempGraphLinks.forEach((link: Link<Node>) => {
-      link.time.forEach((t: number, index: number) => {
-        this.graph.links.push({
-          source: (link.source as number),
-          target: (link.target as number),
-          time: [(index + 1) * t]
-        });
-      });
-    });
-
     this.simulation = d3.forceSimulation<Node>(this.graph.nodes)
-      .force('link', d3.forceLink<Node, Link<Node>>(this.graph.links).distance(SIMULATION_CONFIGURATION.LINK_DISTANCE).strength(SIMULATION_CONFIGURATION.LINK_STRENGTH / NUMBER_OF_TIME_SLICES).id(d => d.id))
+      .force('link', d3.forceLink<Node, Link<Node>>(this.graph.links).distance(SIMULATION_CONFIGURATION.LINK_DISTANCE).strength(SIMULATION_CONFIGURATION.LINK_STRENGTH).id(d => d.id))
       .force('collide', d3.forceCollide().strength(SIMULATION_CONFIGURATION.NODE_STRENGTH).radius(DISPLAY_CONFIGURATION.NODE_RADIUS * 2))
       .force('charge', d3.forceManyBody().strength(SIMULATION_CONFIGURATION.MANYBODY_STRENGTH))
       .force('center', d3.forceCenter(NODE_LINK_SIZE.WIDTH / 2, NODE_LINK_SIZE.HEIGHT / 2).strength(SIMULATION_CONFIGURATION.CENTER_STRENGTH))
@@ -255,63 +210,54 @@ export class NlSiComponent implements OnInit, AfterViewInit {
     // Compute Simulation Based on SUPERGRAPH 💪
     this.simulation.restart();
 
-    this.links = this.g.append('g').attr('class', 'links').selectAll('.link');
     this.nodes = this.g.append('g').attr('class', 'nodes').selectAll('.node');
+    this.links = this.g.append('g').attr('class', 'links').selectAll('.link');
+  }
+
+  update($event: number): void {
+    if (!this.graph) return;
+
+    this.timers.push({
+      type: 'slider',
+      time: 0
+    });
+
+    this.interactions.slider++;
+
+    parent.postMessage({ interactions: this.interactions, timers: this.timers }, '*');
+
+    this.nodes
+      .selectAll('circle')
+      .transition()
+      .duration(TRANSITION_DURATION)
+      .ease(d3.easeCubicOut);
+      // .attr('opacity', (d: any) => {
+      //   if (d.time !== $event) nodesOutOfCurrentTime.add(d.label);
+      //   return d.time === $event ? 1 : 0;
+      // });
+
+    this.nodes
+      .selectAll('text')
+      .transition()
+      .duration(TRANSITION_DURATION)
+      .ease(d3.easeCubicOut);
+      // .attr('opacity', (d: any) => {
+      //   console.log(d);
+      //   if (d.time !== $event) nodesOutOfCurrentTime.add(d.label);
+      //   return d.time === $event ? 1 : 0;
+      // });
+
+    this.links
+      // .selectAll('link')
+      .transition()
+      .duration(TRANSITION_DURATION)
+      .ease(d3.easeCubicOut)
+      .attr('stroke-opacity', (d: Link<Node>) => {
+        return d.time[$event-1];
+      });
   }
 
   init(): void {
-    this.legend = this.legend.data([1]);
-
-    this.legend = this.legend
-      .enter()
-      .append('g');
-
-    for (let i = 1; i <= NUMBER_OF_TIME_SLICES; i++) {
-      this.legend
-        .append('rect')
-        .attr('width', 50)
-        .attr('height', 20)
-        .attr('x', 50 * (i - 1))
-        .attr('y', 0)
-        .attr('fill-opacity', 1)
-        .attr('fill', this.color(i))
-
-      this.legend
-        .append('text')
-        .text(`T${i}`)
-        .attr('font-size', FONT_SIZE)
-        .attr('fill', 'white')
-        .attr('paint-order', 'stroke')
-        .attr('stroke', 'black')
-        .attr('stroke-width', 2)
-        .attr('x',  50 * (i - 1) + (FONT_SIZE * 0.8))
-        .attr('y', (FONT_SIZE * 0.9));
-    }
-
-    // UPDATE
-    this.links = this.links.data(this.graph.links);
-
-    // ENTER
-    this.links = this.links
-      .enter()
-      .append('line')
-      .attr('class', 'link')
-      .attr('stroke', (d: Link<Node>, i: number) => {
-        return d.time[0] ? this.color(d.time[0]) : 'white';
-      })
-      .attr('stroke-opacity', 1)
-      .attr('stroke-width', 2)
-      .attr('time', (d: Link<Node>) => { return d.time[0]; })
-      .on('mouseover', this.mouseOver.bind(this))
-      .on('mouseout', this.mouseOut.bind(this));
-
-    // JOIN
-    this.links = this.links
-      .merge(this.links);
-
-    // EXIT
-    this.links.exit().remove();
-
     // UPDATE
     this.nodes = this.nodes.data(this.graph.nodes);
 
@@ -320,7 +266,7 @@ export class NlSiComponent implements OnInit, AfterViewInit {
       .enter()
       .append('g')
       .attr('class', 'node')
-      .style('cursor', () => { return this.interactionSwitch ? 'pointer' : ''; })
+      .style('cursor',  () => { return this.interactionSwitch ? 'pointer' : ''; })
       .call(this.drag);
 
     this.nodes
@@ -338,7 +284,7 @@ export class NlSiComponent implements OnInit, AfterViewInit {
       .attr('y', (d: Node) => { return d.y + DISPLAY_CONFIGURATION.NODE_RADIUS; })
       .attr('stroke', 'white')
       .attr('stroke-width', 2)
-      .attr('paint-order', 'stroke');
+      .attr('paint-order', 'stroke');;
 
     // JOIN
     this.nodes = this.nodes
@@ -346,38 +292,33 @@ export class NlSiComponent implements OnInit, AfterViewInit {
 
     // EXIT
     this.nodes.exit().remove();
+
+    // UPDATE
+    this.links = this.links.data(this.graph.links);
+
+    // ENTER
+    this.links = this.links
+      .enter()
+      .append('line')
+      .attr('class', 'link')
+      .attr('stroke', 'darkgray')
+      .attr('stroke-opacity', (d: Link<Node>) => { return d.time[0]; })
+      .attr('stroke-width', 1);
+
+    // JOIN
+    this.links = this.links
+      .merge(this.links);
+
+    // EXIT
+    this.links.exit().remove();
   }
 
   render(): void {
     this.links
-      .attr('x1', (d: Link<Node>, i: number) => {
-        let idx = i % 4;
-        let dx = (d.target as Node).x - (d.source as Node).x;
-        let dy = (d.target as Node).y - (d.source as Node).y;
-        let angle = Math.atan2(dy, dx);
-        return (d.source as Node).x + Math.sin(angle - Math.PI) * (idx * 2 - DISPLAY_CONFIGURATION.NODE_RADIUS / 2);
-      })
-      .attr('y1', (d: Link<Node>, i: number) => {
-        let idx = i % 4;
-        let dx = (d.target as Node).x - (d.source as Node).x;
-        let dy = (d.target as Node).y - (d.source as Node).y;
-        let angle = Math.atan2(dy, dx);
-        return (d.source as Node).y + Math.cos(angle) * (idx * 2 - DISPLAY_CONFIGURATION.NODE_RADIUS / 2);
-      })
-      .attr('x2', (d: Link<Node>, i: number) => {
-        let idx = i % 4;
-        let dx = (d.target as Node).x - (d.source as Node).x;
-        let dy = (d.target as Node).y - (d.source as Node).y;
-        let angle = Math.atan2(dy, dx);
-        return (d.target as Node).x + Math.sin(angle - Math.PI) * (idx * 2 - DISPLAY_CONFIGURATION.NODE_RADIUS / 2);
-      })
-      .attr('y2', (d: Link<Node>, i: number) => {
-        let idx = i % 4;
-        let dx = (d.target as Node).x - (d.source as Node).x;
-        let dy = (d.target as Node).y - (d.source as Node).y;
-        let angle = Math.atan2(dy, dx);
-        return (d.target as Node).y + Math.cos(angle) * (idx * 2 - DISPLAY_CONFIGURATION.NODE_RADIUS / 2);
-      });
+      .attr('x1', (d: Link<Node>) => { return (d.source as Node).x; })
+      .attr('y1', (d: Link<Node>) => { return (d.source as Node).y; })
+      .attr('x2', (d: Link<Node>) => { return (d.target as Node).x; })
+      .attr('y2', (d: Link<Node>) => { return (d.target as Node).y; });
 
     this.nodes.selectAll('circle')
       .attr('cx', (d: Node) => { return d.x; })
@@ -387,6 +328,5 @@ export class NlSiComponent implements OnInit, AfterViewInit {
       .attr('x', (d: Node) => { return d.x + DISPLAY_CONFIGURATION.NODE_RADIUS; })
       .attr('y', (d: Node) => { return d.y + DISPLAY_CONFIGURATION.NODE_RADIUS; });
   }
-
 }
 
