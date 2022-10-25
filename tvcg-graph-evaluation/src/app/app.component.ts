c293145d3a756e2962720e88543743e324fa9121
velitchko <velitchko.filipov@tuwien.ac.at>
2022-10-25 12:49:53 +0200

    update title

--- tvcg-graph-evaluation/src/app/app.component.ts
+++ tvcg-graph-evaluation/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'graph-evaluation-two';
+  title = 'tvcg-graph-evaluation';
 }
