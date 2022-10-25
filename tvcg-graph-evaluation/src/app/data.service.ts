ec89756cf4a246ae7ed5f84bfc24852117e4f4c4
velitchko <velitchko.filipov@tuwien.ac.at>
2022-10-25 12:49:46 +0200

    loaded new datasets

--- tvcg-graph-evaluation/src/app/data.service.ts
+++ tvcg-graph-evaluation/src/app/data.service.ts
@@ -1,5 +1,10 @@
 import { Injectable } from '@angular/core';
-import INFOVIS from '../assets/infovis.json'
+import INFOVIS from '../assets/infovis.json';
+import NEWCOMB from '../assets/newcomb.json';
+import PRIDE from '../assets/pride.json';
+import RAMP from '../assets/ramp.json';
+import RUGBY from '../assets/rugby.json';
+import VANDEBUNT from '../assets/vandebunt.json';
 import SFG_EXAMPLE from '../assets/SFG_EXAMPLE.json';
 @Injectable({
   providedIn: 'root'
@@ -18,6 +23,11 @@ export class Graph {
 export class DataService {
 
   private graph_infovis = {};
+  private graph_newcomb = {};
+  private graph_pride = {};
+  private graph_ramp = {};
+  private graph_rugby = {};
+  private graph_vandebunt = {};
 
   // test - example
   private graph_example = {};
@@ -28,12 +38,22 @@ export class DataService {
     console.log(this.graph_infovis);
     // test - example
     this.graph_example = SFG_EXAMPLE;
+    this.graph_newcomb = NEWCOMB;
+    this.graph_pride = PRIDE;
+    this.graph_ramp = RAMP;
+    this.graph_rugby = RUGBY;
+    this.graph_vandebunt = VANDEBUNT;
   }
 
   getGraph(graph: string): Graph {
     
     switch(graph) {
       case 'infovis': return new Graph(this.graph_infovis['nodes'], this.graph_infovis['links']);
+      case 'newcomb': return new Graph(this.graph_newcomb['nodes'], this.graph_newcomb['links']);
+      case 'pride': return new Graph(this.graph_pride['nodes'], this.graph_pride['links']);
+      case 'ramp': return new Graph(this.graph_ramp['nodes'], this.graph_ramp['links']);
+      case 'rugby': return new Graph(this.graph_rugby['nodes'], this.graph_rugby['links']);
+      case 'vandebunt': return new Graph(this.graph_vandebunt['nodes'], this.graph_vandebunt['links']);
       default: return  new Graph(this.graph_example['nodes'], this.graph_example['links']);
     }
   }
