3a4fa863424ef0d3ffdbf31b44738806f1ff927b
velitchko <velitchko.filipov@tuwien.ac.at>
2022-11-10 13:43:58 +0100

    added forms module

--- tvcg-graph-evaluation/src/app/app.module.ts
+++ tvcg-graph-evaluation/src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import {MatGridListModule} from '@angular/material/grid-list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -35,6 +36,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     NgbModule,
     NgxSliderModule,
     MatGridListModule,
+    FormsModule,
     NgxBootstrapIconsModule.pick(allIcons),
     BrowserAnimationsModule
   ],
