be95b94d118212fbebc657725ac8c07c88161fa0
velitchko <velitchko.filipov@tuwien.ac.at>
2022-12-12 10:37:45 +0100

    fixing reordering service and checking of calculations have already been done

--- tvcg-graph-evaluation/src/app/reorder.service.ts
+++ tvcg-graph-evaluation/src/app/reorder.service.ts
@@ -13,19 +13,29 @@ export class ReorderService {
     private reorderGraph: any;
     private reorderMatrix: any;
     public algorithms: Array<String>;
+    public properties: Map<String, string>;
 
     constructor() {
-        this.algorithms = ['none', 'Leaforder', 'LeaforderDist', 'Barycenter', 'RCM', 'Spectral', 'NN2OPT'];
+        this.algorithms = ['none', 'Leaforder', 'LeaforderDist', 'Barycenter', 'RCM', 'Spectral']; // , 'NN2OPT'
+        this.properties = new Map<String, string>();
+
+        this.properties.set('none', 'label');
+        this.properties.set('Leaforder', 'leafOrder');
+        this.properties.set('LeaforderDist', 'leafOrderDist');
+        this.properties.set('Barycenter', 'barycenter');
+        this.properties.set('RCM', 'rcm');
+        this.properties.set('Spectral', 'spectral');
     }
 
     reorder(algorithm: String): any {
         switch (algorithm) {
+            case 'none': return this.reorderGraph.nodes();
             case 'Leaforder': return this.computeLeaforder();
             case 'LeaforderDist': return this.computeLeaforderDist();
             case 'Barycenter': return this.computeBarycenter();
             case 'RCM': return this.computeRCM();
             case 'Spectral': return this.computeSpectral();
-            case 'NN2OPT': return this.computeNN2OPT();
+            // case 'NN2OPT': return this.computeNN2OPT();
             default: undefined;
         }
     }
@@ -46,65 +56,84 @@ export class ReorderService {
     }
 
     computeLeaforder(): any {
+        if(this.reorderGraph.nodes()[0].leafOrder) {
+            console.log('leafOrder already computed');
+            return this.reorderGraph.nodes().slice();
+        }
+
         const order = this.leafOrder(this.adjacency);
 
         order.forEach((lo: any, i: number) => {
             this.reorderGraph.nodes()[i].leafOrder = lo;
         });
 
-        return this.reorderGraph.nodes().map((n: any) => { return n.leafOrder; });
+        return this.reorderGraph.nodes().slice();
     }
 
     computeLeaforderDist(): any {
+        if(this.reorderGraph.nodes()[0].leafOrderDist) {
+            console.log('leafOrderDist already computed');
+            return this.reorderGraph.nodes().slice();
+        }
+
         if (!this.dist_adjacency) this.dist_adjacency = (reorder as any).graph2valuemats(this.reorderGraph);
 
-        // FIXME: something wrong with the function call below - cannot read property 'left' of undefined
         const order = (reorder as any).valuemats_reorder(this.dist_adjacency, this.leafOrder);
 
         order.forEach((lo: any, i: number) => {
             this.reorderGraph.nodes()[i].leafOrderDist = lo;
         });
-        return this.reorderGraph.nodes().map((n: any) => { return n.leafOrderDist; });
+        return this.reorderGraph.nodes().slice();
     }
 
     computeBarycenter(): any {
+        if(this.reorderGraph.nodes()[0].barycenter) {
+            console.log('barycenter already computed');
+            return this.reorderGraph.nodes().slice();
+        }
         const barycenter = (reorder as any).barycenter_order(this.reorderGraph);
         const improved = (reorder as any).adjacent_exchange(this.reorderGraph, barycenter[0], barycenter[1]);
         improved[0].forEach((lo: any, i: number) => {
             this.reorderGraph.nodes()[i].barycenter = lo;
         });
 
-        return this.reorderGraph.nodes().map((n: any) => {
-            // FIXME: barycenter is undefined in the last couple of nodes
-            return n.barycenter;
-        });
+        return this.reorderGraph.nodes().slice();
     }
 
     computeRCM(): any {
+        if(this.reorderGraph.nodes()[0].rcm) {
+            console.log('rcm already computed');
+            return this.reorderGraph.nodes().slice();
+        }
         const rcm = (reorder as any).reverse_cuthill_mckee_order(this.reorderGraph);
         rcm.forEach((lo: any, i: number) => {
             this.reorderGraph.nodes()[i].rcm = lo;
         });
 
-        return this.reorderGraph.nodes().map((n: any) => { return n.rcm; });
+        return this.reorderGraph.nodes().slice();
     }
 
     computeSpectral(): any {
+        if(this.reorderGraph.nodes()[0].spectral) {
+            console.log('spectral already computed');
+            return this.reorderGraph.nodes().slice();
+        } 
         const spectral = (reorder as any).spectral_order(this.reorderGraph);
 
         spectral.forEach((lo: any, i: number) => {
             this.reorderGraph.nodes()[i].spectral = lo;
         });
 
-        return this.reorderGraph.nodes().map((n: any) => { return n.spectral; });
+        return this.reorderGraph.nodes().slice();
     }
 
-    computeNN2OPT(): any {
-        const order = this.leafOrder(this.adjacency);
+    // This is same as leaforder
+    // computeNN2OPT(): any {
+    //     const order = this.leafOrder(this.adjacency);
 
-        order.forEach((lo: any, i: number) => {
-            this.reorderGraph.nodes()[i].leafOrder = lo;
-        });
-        return this.reorderGraph.nodes().map((n: any) => { return n.leafOrder; });
-    }
+    //     order.forEach((lo: any, i: number) => {
+    //         this.reorderGraph.nodes()[i].leafOrder = lo;
+    //     });
+    //     return this.reorderGraph.nodes();
+    // }
 };
