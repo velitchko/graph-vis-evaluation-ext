566e2872794e6b87791baa20379b036d4eeb0b0d
velitchko <velitchko.filipov@tuwien.ac.at>
2022-12-13 13:35:34 +0100

    parsing combined csv implemented

--- tvcg-graph-evaluation/results/index.js
+++ tvcg-graph-evaluation/results/index.js
@@ -1,8 +1,10 @@
 import { parse } from 'csv-parse';
-import { createReadStream, createWriteStream } from 'fs';
+import { createReadStream } from 'fs';
+import fs from 'fs';
 import * as assert from 'node:assert/strict';
 import { parseArgs } from "node:util";
 import { argv } from 'process';
+import converter from 'json-2-csv'
 
 const {
     values: { filename, type },
@@ -23,11 +25,6 @@ const {
 
 const techniqueMap = new Map();
 
-techniqueMap.set('', 'MJP');
-techniqueMap.set('', 'MANC');
-techniqueMap.set('', 'NLJP');
-techniqueMap.set('', 'NLANC');
-
 const answerMap = new Map();
 
 answerMap.set('Strongly Disagree', '1');
@@ -41,16 +38,69 @@ answerMap.set('Not Applicable', '0');
 
 main();
 
-function parseCombined(record) {
-    console.log(record);
-    const [question, answer, count] = record;
-    const newRecord = [question, answer, count];
-    return newRecord;
+function parseCombined(records) {
+    let results = [];
+    // csv headers
+    records[0].forEach((header, index) => {
+        if (header.includes('Matrix with Juxtaposition')) {
+            for (let i = 1; i < records.length - 1; i++) {
+                const answerRow = records[i];
+                const answer = answerMap.get(answerRow[index]);
+                results.push({
+                    technique: 'MJP',
+                    question: header,
+                    answerNum: answer,
+                    answer: answerRow[index],
+                })
+            }
+        }
+
+        if (header.includes('Matrix with Animation and Controls')) {
+            for (let i = 1; i < records.length - 1; i++) {
+                const answerRow = records[i];
+                const answer = answerMap.get(answerRow[index]);
+                results.push({
+                    technique: 'MANC',
+                    question: header,
+                    answerNum: answer,
+                    answer: answerRow[index],
+                })
+            }
+        }
+
+        if (header.includes('Node-Link with Juxtaposition')) {
+            for (let i = 1; i < records.length - 1; i++) {
+                const answerRow = records[i];
+                const answer = answerMap.get(answerRow[index]);
+                results.push({
+                    technique: 'NLJP',
+                    question: header,
+                    answerNum: answer,
+                    answer: answerRow[index],
+                })
+            }
+        }
+
+        if (header.includes('Node-Link with Animation and Controls')) {
+            for (let i = 1; i < records.length - 1; i++) {
+                const answerRow = records[i];
+                const answer = answerMap.get(answerRow[index]);
+                results.push({
+                    technique: 'NLANC',
+                    question: header,
+                    answerNum: answer,
+                    answer: answerRow[index],
+                })
+            }
+        }
+    });
+    return results;
 };
 
-function parseIndividual(record) {
-    const [question, answer, count] = record;
-    const newRecord = [question, answer, count];
+function parseIndividual(records) {
+    records.forEach((record) => {
+        console.log(record);
+    });
     return newRecord;
 }
 
@@ -58,7 +108,7 @@ function main() {
     console.log(`Parsing CSV file...${filename} with ${type} flag`);
 
     const input = createReadStream(`./csvs/${filename}`);
-    const output = createWriteStream(`./csvs/${filename.split('.')[0]}-${type}-parsed.csv`, { flags: 'w', encoding: 'utf-8' });
+    // const output = createWriteStream(`./csvs/${filename.split('.')[0]}-${type}-parsed.csv`, { flags: 'w', encoding: 'utf-8' });
 
     const parser = parse({ delimiter: ',' });
     let data = [];
@@ -76,13 +126,21 @@ function main() {
             } else {
                 parsed = parseIndividual(data);
             }
+            converter.json2csv(parsed, (err, csv) => {
+                if (err) {
+                    throw err
+                }
+                // print CSV string
+                console.log(csv)
 
-            output.write(parsed);
+                // write CSV to a file
+                fs.writeFileSync(`./csvs/${filename.split('.')[0]}-${type}-parsed.csv`, csv);
+            });
         })
-        .pipe(output)
-        .on('finish', function () {
-            console.log('Done! 💪 Check the output file.');
-        });
+    // .pipe(output)
+    // .on('finish', function () {
+    //     console.log('Done! 💪 Check the output file.');
+    // });
 }
 
 
