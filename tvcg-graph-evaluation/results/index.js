9e1cc7da81d7338840cbf40d25d268a656a8459c
velitchko <velitchko.filipov@tuwien.ac.at>
2023-01-02 10:49:59 +0200

    individual results parsing + question categories

--- tvcg-graph-evaluation/results/index.js
+++ tvcg-graph-evaluation/results/index.js
@@ -34,6 +34,31 @@ answerMap.set('Agree', '6');
 answerMap.set('Strongly Agree', '7');
 answerMap.set('Not Applicable', '0');
 
+const questionCat = new Map();
+
+questionCat.set('The visualization exposes individual data cases and their attributes', 'Insight');
+questionCat.set('The visualization facilitates perceiving relationships in the data like patterns & distributions of the variables', 'Insight');
+questionCat.set('The visualization promotes exploring relationships between individual data cases as well as different groupings of data cases', 'Insight');
+questionCat.set('The visualization helps generate data-driven questions', 'Insight');
+questionCat.set('The visualization helps identify unusual or unexpected, yet valid, data characteristics or values', 'Insight');
+questionCat.set('The visualization provides useful interactive capabilities to help investigate the data in multiple ways', 'Insight');
+questionCat.set('The visualization shows multiple perspectives about the data', 'Insight');
+questionCat.set('The visualization uses an effective representation of the data that shows related and partially related data cases', 'Insight');
+
+questionCat.set('The visualization uses meaningful and accurate visual encodings to represent the data', 'Confidence');
+questionCat.set('The visualization avoids using misleading representations', 'Confidence');
+questionCat.set('If there were data issues like unexpected, duplicate, missing, or invalid data, the visualization would highlight those issues', 'Confidence');
+
+questionCat.set('The visualization provides a comprehensive and accessible overview of the data', 'Essence');
+questionCat.set('The visualization presents the data by providing a meaningful visual schema', 'Essence');
+questionCat.set('The visualization facilitates generalizations and extrapolations of patterns and conclusions', 'Essence');
+
+questionCat.set('The visualization provides a meaningful spatial organization of the data', 'Time');
+questionCat.set('The visualization shows key characteristics of the data at a glance', 'Time');
+questionCat.set('The visualization supports smooth transitions between different levels of detail in viewing the data', 'Time');
+questionCat.set('The visualization avoids complex commands and textual queries by providing direct interaction with the data representation', 'Time');
+
+
 main();
 
 function parseCombined(records) {
@@ -49,6 +74,7 @@ function parseCombined(records) {
                     question: header,
                     answerNum: answer,
                     answer: answerRow[index],
+                    category: questionCat.get(header.split('[')[0].trim())
                 })
             }
         }
@@ -62,6 +88,7 @@ function parseCombined(records) {
                     question: header,
                     answerNum: answer,
                     answer: answerRow[index],
+                    category: questionCat.get(header.split('[')[0].trim())
                 })
             }
         }
@@ -75,6 +102,7 @@ function parseCombined(records) {
                     question: header,
                     answerNum: answer,
                     answer: answerRow[index],
+                    category: questionCat.get(header.split('[')[0].trim())
                 })
             }
         }
@@ -88,6 +116,7 @@ function parseCombined(records) {
                     question: header,
                     answerNum: answer,
                     answer: answerRow[index],
+                    category: questionCat.get(header.split('[')[0].trim())
                 })
             }
         }
@@ -96,10 +125,73 @@ function parseCombined(records) {
 };
 
 function parseIndividual(records) {
-    records.forEach((record) => {
-        console.log(record);
+    let technique = '';
+    let results = [];
+
+    records[0].forEach((header, index) => {
+        if(records[1][1].includes('Matrix with Juxtaposition')) {
+            technique = 'MJP';
+            console.log(records.length)
+            for (let i = 1; i <= records.length - 1; i++) {
+                    if(index <= 1) return;
+                    const answerRow = records[i];
+                    const answer = answerMap.get(answerRow[index]);
+                    results.push({
+                        technique: technique,
+                        question: header,
+                        answerNum: answer,
+                        answer: answerRow[index],
+                        category: questionCat.get(header.trim())
+                    });
+                }
+        }
+        if(records[1][1].includes('Matrix with Animation and Controls')) {
+            technique = 'MANC';
+            for (let i = 1; i <= records.length - 1; i++) {
+        
+                const answerRow = records[i];
+                    const answer = answerMap.get(answerRow[index]);
+                    results.push({
+                        technique: technique,
+                        question: header,
+                        answerNum: answer,
+                        answer: answerRow[index],
+                        category: questionCat.get(header.trim())
+                    });
+                }
+        }
+        if(records[1][1].includes('Node-Link with Juxtaposition')) {
+            technique = 'NLJP';
+            for (let i = 1; i <= records.length - 1; i++) {
+        
+                const answerRow = records[i];
+                    const answer = answerMap.get(answerRow[index]);
+                    results.push({
+                        technique: technique,
+                        question: header,
+                        answerNum: answer,
+                        answer: answerRow[index],
+                        category: questionCat.get(header.trim())
+                    });
+                }
+        }
+        if(records[1][1].includes('Node-Link with Animation and Controls')) {
+            technique = 'NLANC';
+            for (let i = 1; i <= records.length - 1; i++) {
+        
+                const answerRow = records[i];
+                    const answer = answerMap.get(answerRow[index]);
+                    results.push({
+                        technique: technique,
+                        question: header,
+                        answerNum: answer,
+                        answer: answerRow[index],
+                        category: questionCat.get(header.trim())
+                    });
+                }
+        }
     });
-    return newRecord;
+    return results;
 }
 
 function main() {
